34453b0dc155a6b911bbc4bebfd6a0f4850fb776
Nuwan Tissera <nuwan.tissera@my.sliit.lk>
2018-06-17 12:36:20 +0530

    Inline Editor Code UI

--- public/js/editor.js
+++ public/js/editor.js
@@ -2,31 +2,6 @@ var CodeFlask = require('codeflask');
 
 var JavaScriptObfuscator = require('javascript-obfuscator');
 
-// code for obfusfaction
-
-var obfuscationResult = JavaScriptObfuscator.obfuscate(
-    `
-        (function(){
-            var variable1 = '5' - 3;
-            var variable2 = '5' + 3;
-            var variable3 = '5' + - '2';
-            var variable4 = ['10','10','10','10','10'].map(parseInt);
-            var variable5 = 'foo ' + 1 + 1;
-            console.log(variable1);
-            console.log(variable2);
-            console.log(variable3);
-            console.log(variable4);
-            console.log(variable5);
-        })();
-    `,
-    {
-        compact: false,
-        controlFlowFlattening: true
-    }
-);
-
-console.log(obfuscationResult.getObfuscatedCode());
-
 
 //code for editor
 
@@ -38,6 +13,23 @@ flask.onUpdate((code) => {
 });
 
 const code = flask.getCode();
+
+
+// code for obfuscation
+
+var obfuscate = $('#obfuscate');
+obfuscate.click(function () {
+    var obfuscationResult = JavaScriptObfuscator.obfuscate(
+        flask.getCode(),
+        {
+            compact: false,
+            controlFlowFlattening: true
+        }
+    );
+
+    console.log("Obfuscated Code : " + obfuscationResult.getObfuscatedCode());
+});
+
 
 
 //code for function calling
