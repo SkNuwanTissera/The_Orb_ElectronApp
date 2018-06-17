60c2f740056f85018c9ab8ec4d4fd393b0b2c5a4
Nuwan Tissera <nuwan.tissera@my.sliit.lk>
2018-06-17 07:50:39 +0530

    Inline Editor Code UI

--- editor.js
+++ editor.js
@@ -7,3 +7,30 @@ flask.onUpdate((code) => {
     console.log("This is "+code);
 });
 
+const code = flask.getCode();
+
+
+var net = require('electron')('net');
+var send = $('#send');
+send.click(function() {
+    alert( "Handler for .click() called." );
+    get();
+});
+
+
+function get(){
+    const request = net.request({
+        method: 'GET',
+        protocol: 'https:',
+        hostname: 'github.com',
+        port: 443,
+        path: '/'
+    });
+
+    request.on('response', (response) => {
+        console.log(`STATUS: ${response.statusCode}`);
+        response.on('error', (error) => {
+            console.log(`ERROR: ${JSON.stringify(error)}`)
+        })
+    });
+}
--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const url = require('url')
 
 let win
 
+
 function createWindow () {
     // Create the browser window.
     win = new BrowserWindow({width: 900, height: 600})
