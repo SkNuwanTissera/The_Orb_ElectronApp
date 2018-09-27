e06ea653e9d44649e7663e428489aca79545d97a
Achala Kavinda <achalakavinda25r@gmail.com>
2018-09-27 21:22:01 +0530

    implement exception handling in request service

--- public/services/request.service.js
+++ public/services/request.service.js
@@ -3,33 +3,40 @@ var electron_remote = require('electron').remote;// this module initiate pipe wi
 
 var SocUri = "";
 
+const boostrapUri = "http://192.168.1.5:4500";
+
 module.exports = {
 
     Request : function () {
-        request('http://192.168.1.9:4500', function (error, response, body) {
+        request(boostrapUri, function (error, response, body) {
 
-            if(!error){
-                obj = JSON.parse(body);
-                var uri = "";
-                var latency = 0;
+            try {
+                if(!error){
+                    obj = JSON.parse(body);
+                    var uri = "";
+                    var latency = 0;
 
-                //need to implement resolving algo
+                    //need to implement resolving algo
 
-                obj.forEach(data=>{
-                    console.log("Online Supernodes : "+data.uri)
-                    if(latency<data.latency){
-                        latency = data.latency;
-                        uri = data.uri;
+                    obj.forEach(data=>{
+                        console.log("Online Supernodes : "+data.uri)
+                        if(latency<data.latency){
+                            latency = data.latency;
+                            uri = data.uri;
+                        }
+                    });
+
+                    if(uri.length>0){
+                        console.log('resolve super node uri : '+uri);
+                        SocUri  = uri.toString();
+                    }else {
+                        console.log('zero super nodes')
                     }
-                });
 
-                if(uri.length>0){
-                    console.log('resolve super node uri : '+uri);
-                    SocUri  = uri.toString();
-                }else {
-                    console.log('zero super nodes')
                 }
-
+            }catch (e) {
+                console.log("Request Service | Request | Exception : "+e)
+                console.log("check boostrap server Ip Address : trying to connect => "+boostrapUri)
             }
         });
     },
